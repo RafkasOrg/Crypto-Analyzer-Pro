39c16a9c41307d05b55b11929bed0b4d3c6a6d43
RafkasOrg <rafka12122018@gmail.com>
2025-08-01 02:04:59 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,7 @@
 // dashboard.js
 
 // Supabase config
-const supabaseUrl = https://pppcusoyjkvlsfdurgpv.supabase.co; // Ganti sesuai proyek kamu
+const supabaseUrl = 'https://pppcusoyjkvlsfdurgpv.supabase.co'; // Ganti sesuai proyek kamu
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwcGN1c295amt2bHNmZHVyZ3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5Nzk3NDAsImV4cCI6MjA2OTU1NTc0MH0.PJOY8puQcps88f0e9ZyS2-ol1Zmm6y7p8zKJSgsQcho'; // Ganti sesuai key kamu
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
